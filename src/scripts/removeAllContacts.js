fccf38497bdeec6e89f16a0274c70180377def2b
kopasovska <v.kopasovska@gmail.com>
2025-05-29 21:29:54 +0200

    Finish node.js first homework

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,10 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    const data = await readContacts();
+    data.length = 0;
+    await writeContacts(data);
+};
 
 removeAllContacts();
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const buffer = await fs.readFile(PATH_DB, 'utf8');
+        return JSON.parse(buffer);
+    } catch (error) {
+        console.log(error);
+    }
+};
